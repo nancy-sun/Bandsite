7d9e8b64030546d2cabee003939b741325293855
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-05-12 01:59:25 -0700

    add js form submission

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,9 +1,11 @@
-const defaultComment = [
+
+//default comment
+const comments = [
     {
         name: "Connor Walton",
         timestamp: "02/17/2021",
         text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-        avatar: "../assets/Images/Mohan-muruge.jpg" //url path
+        avatar: "./assets/images/mohan-muruge.jpg" //url path
     },
     {
         name: "Emilie Beach",
@@ -19,49 +21,90 @@ const defaultComment = [
     }
 ]
 
+
 function displayComment(commentsArr) {
     for (let i = 0; i < commentsArr.length; i++) {
-        createCommentContent(commentsArr[i]);
+        createComment(commentsArr[i]);
     }
 }
 
+//grab the parent <ul> element
 const commentList = document.querySelector(".comment__list");
 
-// original function with everything
-function createCommentContent(commentObj) {
-    const commentBox = document.createElement("li");
-    commentList.appendChild(commentBox);
-    commentBox.classList.add("comment__box");
+//create element and give it a class
+function createCommentElement(tag, className) {
+    const item = document.createElement(tag);
+    item.classList.add(className);
+    return item;
+}
 
-    const avatar = document.createElement("div");
-    avatar.classList.add("comment__avatar");
+function createComment(commentObj) {
+    const commentBox = createCommentElement("li", "comment__box");
+    commentList.appendChild(commentBox);
+
+    const avatar = createCommentElement("div", "comment__avatar");
     avatar.classList.add("avatar");
-    avatar.style.backgroundImage = `url(${commentObj.avatar})`
+    avatar.style.backgroundImage = `url(${commentObj.avatar})`;
     commentBox.appendChild(avatar);
 
-    const context = document.createElement("div");
-    context.classList.add("comment__context");
+    const context = createCommentElement("div", "comment__context");
     commentBox.appendChild(context);
 
-    const info = document.createElement("div");
-    info.classList.add("comment__info");
+    const info = createCommentElement("div", "comment__info");
     context.appendChild(info);
 
-    const name = document.createElement("p")
-    name.classList.add("comment__name");
+    const name = createCommentElement("p", "comment__name");
     name.innerText = commentObj.name;
     info.appendChild(name);
 
-    const time = document.createElement("p");
-    time.classList.add("comment__time");
+    const time = createCommentElement("p", "comment__time");
     time.innerText = commentObj.timestamp;
     info.appendChild(time);
 
-    const text = document.createElement("p");
-    text.classList.add("comment__text");
+    const text = createCommentElement("p", "comment__text");
     text.innerText = commentObj.text;
     context.appendChild(text);
 }
 
 
-displayComment(defaultComment);
+//form submission
+
+//grab the <form>
+const form = document.querySelector(".comment__form");
+
+//grab the submit button
+const submit = document.querySelector(".comment__submit");
+
+//get submission date
+function getTimestamp() {
+    const today = new Date();
+    let month = today.getMonth() + 1;
+    if (month < 10) {
+        month = "0" + month;
+    };
+    const date = today.getDate();
+    const year = today.getFullYear();
+    return month + "/" + date + "/" + year;
+}
+
+displayComment(comments);
+
+//when submit form
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const today = getTimestamp();
+    const newComment = {
+        name: event.target.name.value,
+        timestamp: today,
+        text: event.target.comment.value,
+        avatar: "#" //url path
+    };
+    form.reset();
+    // const newComments = comments;
+    comments.unshift(comments); //remove the previous comments
+    displayComment(comments);
+});
+
+
+
+
